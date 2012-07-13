89361e22bc36fa7b06fa7f385260ab3abfa6afd6
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2012-07-13 13:39:12 -0500

    delete iteration file

--- it_protclust.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ln -s $1 iteration1.fas
-for i in {1..10}
-do
-	#echo iteration${i}.fas  iteration`expr $i + 1`.fas
-	./prot_cluster iteration${i}.fas | tee iteration${i}.log
-	cat iteration${i}.fas.consensus iteration1.fas  > iteration`expr $i + 1`.fas
-done
