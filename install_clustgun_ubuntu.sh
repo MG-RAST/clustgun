1dff86f17fac1740f31a8fc908d7af62c81e797a
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2013-03-08 16:44:24 -0600

    install script for ubuntu

---
+++ install_clustgun_ubuntu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+sudo apt-get install libboost-system-dev libboost-iostreams-dev libboost-program-options-dev
+
+if [ -d ~/clustgun ]; then
+	cd ~/clustgun
+	git pull
+	make clean
+else
+	cd
+	git clone git://github.com/MG-RAST/clustgun.git
+fi
+
+cd ~/clustgun
+
+make
+
+sudo ln -s /home/ubuntu/clustgun/clustgun /usr/bin/clustgun
+
